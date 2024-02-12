41b13a953899ad4587433f1eba274db498e35654
Ilya Sukhan <126617855+kolen44@users.noreply.github.com>
2024-02-12 08:53:10 +0000

    feat: added login form A login form has been added that is responsible for future login. The form was made using react-hook-form and also using Zod. The corresponding file is responsible for the form - login-form, which is located at components/auth/login-form.jsx. A schema was created for the form, which is located in the schemas/index.js directory. The schema is responsible for the validity of the form.

--- src/components/auth/card-wrapper.jsx
+++ src/components/auth/card-wrapper.jsx
@@ -1,9 +1,28 @@
 'use client'
+import Header from '@/components/auth/header'
+import Social from './social'
+import BackButton from './back-button'
 import {Card, CardHeader, CardFooter, CardContent} from '.././ui/card'
 export default function CardWrapper({children, headerLabel, backButtonLabel, backButtonHref, showSocial}){
 	return(
 		<Card className='w-[400px] shadow-md'>
-			{children}
+			<CardHeader>
+				<Header label={headerLabel}/>
+			</CardHeader>
+			<CardContent>
+				{children}
+			</CardContent>
+			{showSocial && (
+				<CardFooter>
+					<Social />
+				</CardFooter>
+			)}
+			<CardFooter>
+				<BackButton 
+					label={backButtonLabel}
+					href={backButtonHref}
+				/>
+			</CardFooter>
 		</Card>
 	)
 }
--- src/components/auth/login-form.jsx
+++ src/components/auth/login-form.jsx
@@ -1,9 +1,92 @@
+'use client'
 import CardWrapper from './card-wrapper'
+import * as z from 'zod'
+import {useForm} from 'react-hook-form'
+import {zodResolver} from '@hookform/resolvers/zod'
+import { LoginSchema } from '../../../schemas'
+import {Input} from '@/components/ui/input'
+import {Button} from '../ui/button'
+
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage
+} from '@/components/ui/form'
 
 export default function LoginForm(){
+	const form = useForm({
+		resolver: zodResolver(LoginSchema),
+		defaultValues: {
+			email: "",
+			password: "",
+		}
+	})
+
+	const onSubmit = (values) => {
+		console.log(values)
+	}
+
 	return(
-		<CardWrapper>
-			LoginForm
+		<CardWrapper 
+			showSocial={true} 
+			headerLabel={'Welcome back'}
+			backButtonHref="/auth/register"
+			backButtonLabel={`Don't have an account?(`}
+		>
+			<Form {...form}>
+				<form onSubmit={form.handleSubmit(onSubmit)}
+					className='space-y-6'
+				>
+					<div className='space-y-4'>
+						<FormField
+							control={form.control}
+							name='email'
+							render={({field, }) => (
+								<FormItem>
+									<FormLabel>Email</FormLabel>
+									<FormControl>
+										<Input 
+											{...field}
+											placeholder='pups@gmail.com'
+											type='email'
+										/>
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						>
+						</FormField>
+
+						<FormField
+							control={form.control}
+							name='password'
+							render={({field, }) => (
+								<FormItem>
+									<FormLabel>Password</FormLabel>
+									<FormControl>
+										<Input 
+											{...field}
+											placeholder='1234qwerty'
+											type='password'
+										/>
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						>
+						</FormField>
+					</div>
+					<Button
+						type='submit'
+						className='w-full'
+					>
+						Login
+					</Button>
+				</form>
+			</Form>
 		</CardWrapper>
 	)
 }
